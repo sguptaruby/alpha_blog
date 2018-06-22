ec2b9b7b46b5619583e1a372a5508225cf0e8905
sagar <sagargupta709@gmail.com>
2018-06-23 01:38:49 +0530

    add update edit article funcationality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,44 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def create
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+
+        if @article.save
+        flash[:notice] = "Article was successful create."
+        redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def index
+        @article = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article edit successfuly."
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def article_params
+        #params.require:(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description)
+    end
 end
